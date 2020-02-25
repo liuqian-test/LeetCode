318f3c1777b654cf9b69ae1c1cac630a176e0ce1
liuqian-test <1254143911@qq.com>
2020-02-25 20:53:24 +0800

    this is hello code

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+/***********************************************************
+*       &Author: liu_qian     
+*       &E-mail: liu_qian@foxmail.com
+*       &Motto: Believe in yourself.
+*       &File Name: hello.cpp
+*       &Created Time: 20Century 2020年02月25日 星期二 20时51分29秒 
+*                          CST  Day/056 and Week/08 of this year
+*       &Description:
+***********************************************************/
+
+
+#include <stdio.h>
+
+int main() {
+
+	printf("hello");
+	return 0;
+}
