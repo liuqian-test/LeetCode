0eb1ffb9e353cbabd5cecda3e207cf2cbbc8e004
liuqian-test <1254143911@qq.com>
2020-03-10 12:54:09 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/***********************************************************
+*       &Author: liu_qian     
+*       &E-mail: liu_qian@foxmail.com
+*       &Motto: Believe in yourself.
+*       &File Name: test.cpp
+*       &Created Time: 20Century 2020年03月10日 星期二 12时50分40秒 
+*                          CST  Day/070 and Week/10 of this year
+*       &Description:
+***********************************************************/
+
+
+#include <stdio.h>
+
+int main () {
+	printf("%d", 888);
+	return 0;
+}
